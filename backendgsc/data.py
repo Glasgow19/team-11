e811b15ba39163ba0f79e023095574d90e1e0c94
Sarah Houcheimi <Sarah.houcheimi@gmail.com>
2019-10-26 23:30:19 +0100

    fix everything

--- backendgsc/data.py
+++ backendgsc/data.py
@@ -1,90 +1,51 @@
-import graphene
+import requests
+from datetime import datetime
 
-class gammaEvents(graphene.ObjectType):
-    # id = graphene.ID()
-    # name = graphene.String()
-    # age = graphene.Int()
-    date = graphene.String()
-    category = graphene.String()
-    ResName = graphene.String()
-    SubCategory = graphene.String()
-    SourceResViews = graphene.String()
-    ResDate = graphene.String()
-    StartTime = graphene.String()
-    EndTime = graphene.String()
-    Available = graphene.String()
-    TotalBooked = graphene.String()
-    Capacity = graphene.String()
-    Description = graphene.String()
-    Area = graphene.String()
-    RunningTime = graphene.String()
-    Age = graphene.String()
-    ShowType = graphene.String()
-    FilmCertification = graphene.String()
-    Price = graphene.String()
-    InformationPage = graphene.String()
-    GammaResponseType = graphene.String()
-    ResID = graphene.String()
-    ProductMapID = graphene.String()
-    lastSync = graphene.String()
-def __repr__(self):
-    reutn(str(self.TotalBooked))
-    #return "<__main__.Camera: distance = " + str(self.distance) + "; speed_limit = " + str(self.speed_limit) + "; number_of_cars = " + str(self.number_of_cars) + ">"
-
-class Query(graphene.ObjectType):
-    #print("I am in the Query Class")
-    event = graphene.Field(gammaEvents)
-    #print(event)
-    gE = gammaEvents(date = "2019-10-26", category = "Public")
+headers = {"Authorization": "Bearer YOUR API KEY"}
 
 
-    def resolve_event(self, info):
-        print("I am in the resolve_event")
-        return gammaEvents(date = "2019-10-26", category = "Public")
-
-    # hello = graphene.String(description='A typical hello world')
-    #
-    # def resolve_hello(self, info):
-    #     return 'World'
+def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+    request = requests.post('https://gammaql.gsc.org.uk/', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
 
-schema = graphene.Schema(query=Query)
+# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.
 query = """
-    query getTodayEvents{
-        gammaEvents{
-            ResName
-            SubCategory
-            SourceResViews
-            ResDate
-            StartTime
-            EndTime
-            Available
-            TotalBooked
-            Capacity
-            Description
-            Area
-            RunningTime
-            Age
-            ShowType
-            FilmCertification
-            Price
-            InformationPage
-            GammaResponseType
-            ResID
-            ProductMapID
-            lastSync
-          }
-        }
+{
+    gammaEvents{
+        ResName
+        Category
+        SubCategory
+        SourceResViews
+        ResDate
+        StartTime
+        EndTime
+        Available
+        TotalBooked
+        Capacity
+        Description
+        Area
+        RunningTime
+        Age
+        ShowType
+        FilmCertification
+        Price
+        InformationPage
+        GammaResponseType
+        ResID
+        ProductMapID
+        lastSync
+      }
+    }
 """
 
-
-# def test_query():
-#     result = schema.execute(query)
-#     assert not result.errors
-#     assert result.data == {"event": {"id": "1", "name": "Syrus", "age": 27}}
-
-
-if __name__ == "__main__":
-    result = schema.execute(query)
-    print(str(result))
-    #print(result.data["event"])
+result = run_query(query) # Execute the query
+list = result["data"]["gammaEvents"]
+today = datetime.date(datetime.now())
+location = "Planetarium"
+for event in list:
+    if (event["ResDate"] == str(today) and event["Category"] == "Public" and event["Area"] == location):
+        print(event["ResName"])
