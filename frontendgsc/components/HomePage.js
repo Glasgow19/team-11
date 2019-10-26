51a44a07c41fb02ba77a8dce8a8bfa73bc499bbd
Patricia milou <patriciamilou97@gmail.com>
2019-10-26 16:06:11 +0100

    camera added

--- frontendgsc/components/HomePage.js
+++ frontendgsc/components/HomePage.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Camera from './Camera'
 
 const HomePage = (props) => {
+    // process.nextTick = setImmediate;
     return (
-        <View>
-            <Text>
-                Hello from Home Page.
-            </Text>
+        <View style={styles.container}>
+            <Camera/>
         </View>
     );
 }
 
 export default HomePage;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',	
+	}
+});
