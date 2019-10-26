cce6746daca62a9c5098387eda97d9984fcf0b2f
Anguel <angel.hristozov@gmail.com>
2019-10-26 15:10:52 +0100

    basic backend setup

---
+++ backendgsc/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return f'index'
+
+
+@app.route('/exhibition/info', methods=['GET', 'POST'])
+def exhibition_info():
+    if request.method == 'GET':
+        # get already existing exhibition
+        return
+    else:
+        # process the image or whatever is sent to it
+        return
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
