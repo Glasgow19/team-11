b09509b307251f33ae38de38a8e1de56c9895b8f
Patricia milou <patriciamilou97@gmail.com>
2019-10-26 21:28:36 +0100

    frontend:swipe

--- frontendgsc/components/LanguagePage.js
+++ frontendgsc/components/LanguagePage.js
@@ -1,17 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { View, StyleSheet,Text,Image } from 'react-native';
 import {Card,CardItem,Bottom,Icon} from 'native-base';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+
 const styles = StyleSheet.create({
     image: {
       width: 300,
-      resizeMode: "stretch"
+      height:170,
+      resizeMode: "stretch",
+      margin:0,
     }
-});
+}); 
+  
 
 const LanguagePage = (props) => {
-    var languages = ["English","French","German","Spannish"]
-    var position = 0
+    var languages = ["English","French","German","Spanish"]
+    var [ position, setPosition ] = useState(0);
+    var [ language, setLanguage ] = useState(languages[position]);
+    const images = {
+        english: {
+          uri: require('../assets/english.png')
+        },
+        french: {
+            uri: require('../assets/french.png')
+        },
+        german: {
+            uri: require('../assets/german.png')
+        },
+        spanish: {
+            uri: require('../assets/spanish.png')
+        }
+      }
+    const config = {
+        velocityThreshold: 0.1,
+        directionalOffsetThreshold: 200
+    };
+    const onSwipeUp = (gestureState) => {
+        if (position == languages.length-1 ){
+            setPosition(0);
+        }else{
+            setPosition(position+1);
+        }
+        setLanguage(languages[position]);
+    }
+     
+    const onSwipeDown = (gestureState) => {
+        if (position == 0 ){
+            setPosition(languages.length-1);
+        }else{
+            setPosition(position-1);
+        }
+        setLanguage(languages[position]);
+        // setPath(language);
+    }
+     
+    // const onSwipe = (gestureName, gestureState) => {
+    //     const {SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections;
+    //     setGestureName(gestureName);
+    //     switch (gestureName) {
+    //       case SWIPE_UP:
+    //         setbgcolor("red");
+    //         break;
+    //       case SWIPE_DOWN:
+    //         setbgcolor("green");
+    //         break;
+    //     }
+    //   }
+
     return (
         <View style={{ flex: 12,backgroundColor:"#131313",alignContent:"space-between",paddingVertical:100,paddingHorizontal:30,}}>
              <View style={{ flex: 1}}>
@@ -21,17 +77,28 @@ const LanguagePage = (props) => {
              </View>
 
              <View style={{ flex: 2}}>
-                 <Card style={{borderColor:"#131313"}}>
-                 <CardItem style={{flexDirection: "column", justifyContent:"center",alignItems:"center",backgroundColor:"#131313"}}>
-                    <Image
-                    style={styles.image}
-                    source={require('../assets/english.png')}
-                    />
-                    <Text style= {{color:"#F1F1F1",fontSize:30,textAlign:"center",fontFamily:"Roboto Condensed"}}>
-                        English
-                    </Text>
-                </CardItem>
-                 </Card>
+                <GestureRecognizer
+                    // onSwipe={onSwipe}
+                    onSwipeUp={onSwipeUp}
+                    onSwipeDown={onSwipeDown}
+                    config={config}
+                    style={{
+                        flex: 1,
+                        backgroundColor: "#131313"
+                    }}
+                    >
+                    <Card style={{borderColor:"#131313"}}>
+                    <CardItem style={{flexDirection: "column", justifyContent:"center",alignItems:"center",backgroundColor:"#131313"}}>
+                        <Image
+                        style={styles.image}
+                        source={images[language.toLowerCase()].uri} 
+                        />
+                        <Text style= {{color:"#F1F1F1",fontSize:30,textAlign:"center",fontFamily:"Roboto Condensed"}}>
+                            {language}
+                        </Text>
+                    </CardItem>
+                    </Card> 
+                </GestureRecognizer>                 
 
              </View>
         </View>
