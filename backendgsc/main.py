3f905e1c8bdcbf446f757d06ef109d9a49c5e94e
Anguel <angel.hristozov@gmail.com>
2019-10-26 21:08:49 +0100

    parse request body

--- backendgsc/main.py
+++ backendgsc/main.py
@@ -24,11 +24,33 @@ def image():
 #     save image to local dir. assume jpg as phones capture jpg
     img = request.files['image']
     try:
+        # todo: convert to base64
         pil_img = Image.open(img)
         pil_img.save('saved_images/' + str(uuid4()) + '.jpg')
         return Response('save success\n', 200)
     except Exception as e:
         return Response('save fail\n', 500)
+
+
+# list of objects
+# { [
+#   name of item: string
+#   topLeft, topRight, bottomLeft, bottomRight coordinates where item is located: float/double
+# ] imageWidth, imageHeight }
+# find out in which section of the image is the item in
+@app.route('/describe', methods=['POST'])
+def describe():
+    req_data = request.get_json()
+    img_height = req_data['height']
+    img_width = req_data['width']
+    half_height = img_height / 2
+    half_width = img_width / 2
+    top_left = req_data['topLeft']
+    top_right = req_data['topRight']
+    bottom_left = req_data['bottomLeft']
+    bottom_right = req_data['bottomRight']
+    # rip these if statements
+
 
 
 if __name__ == '__main__':
