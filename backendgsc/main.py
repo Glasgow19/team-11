39749a710483156e9d774e01866dadbe14e446a2
Anguel <angel.hristozov@gmail.com>
2019-10-27 00:17:30 +0100

    return item

--- backendgsc/main.py
+++ backendgsc/main.py
@@ -24,13 +24,14 @@ def in_center(tl, tr, bl, br, h, w):
     # todo: this
     return False
 
-def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
-    request = requests.post('https://gammaql.gsc.org.uk/', json={'query': query}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
+def run_query(query):  # A simple function to use requests.post to make the API call. Note the json= section.
+    headers = {"Authorization": "Bearer YOUR API KEY"}
+    req = requests.post('https://gammaql.gsc.org.uk/', json={'query': query}, headers=headers)
+    if req.status_code == 200:
+        return req.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(req.status_code, query))
 
 
 @app.route('/exhibition/info', methods=['GET', 'POST'])
@@ -57,7 +58,8 @@ def image():
     except Exception as e:
         return Response('save fail\n', 500)
 
-@app.route('/data', method=['GET'])
+
+@app.route('/data', methods=['GET'])
 def data():
     query = """
     {
@@ -88,17 +90,16 @@ def data():
         }
     """
 
-    result = run_query(query) # Execute the query
+    result = run_query(query)  # Execute the query
     list = result["data"]["gammaEvents"]
     today = datetime.date(datetime.now())
     location = "Planetarium"
     listOfEvents = []
     for event in list:
         if (event["ResDate"] == str(today) and event["Category"] == "Public" and event["Area"] == location):
-            #print(event["ResName"])
+            # print(event["ResName"])
             listOfEvents.append(event["ResName"])
     return listOfEvents
-
 
 
 # list of objects
@@ -109,41 +110,41 @@ def data():
 # find out in which section of the image is the item in
 @app.route('/describe', methods=['POST'])
 def describe():
+    resp = dict()
     req_data = request.get_json()
-    name = req_data['name']
-    img_height = req_data['height']
-    img_width = req_data['width']
-    half_height = img_height / 2
-    half_width = img_width / 2
-    # [x, y]
-    top_left = req_data['topLeft']
-    top_right = req_data['topRight']
-    bottom_left = req_data['bottomLeft']
-    bottom_right = req_data['bottomRight']
-    return_string = "There is {} in your {}."
+    for item in req_data['data']:
+        name = item['name']
+        img_height = item['height']
+        img_width = item['width']
+        half_height = img_height / 2
+        half_width = img_width / 2
+        # [x, y]
+        top_left = item['topLeft']
+        top_right = item['topRight']
+        bottom_left = item['bottomLeft']
+        bottom_right = item['bottomRight']
+        return_string = "There is {} in your {}."
 
-    # check to see if in center
-    # if in_center(top_left, top_right, bottom_left, bottom_right):
-    #     return jsonify({'response': return_string.format(name, 'center')})
+        # rip these if statements. very rudimentary way to find out in which quadrant the object is in
+        if bottom_right[0] < half_width and bottom_right[1] < half_height:
+            # completely top left as furthest down coordinate is before 0,0 in coordinate plane
+            resp[name] = return_string.format(name, 'top left')
+        elif bottom_left[0] >= half_width and bottom_left[1] <= half_height:
+            # completely top right as furthest down coordinate is after 0,0
+            resp[name] = return_string.format(name, 'top right')
+        elif top_left[0] >= half_width and top_left[1] >= half_height:
+            # completely bottom right
+            resp[name] = return_string.format(name, 'bottom right')
+        elif top_right[0] < half_width and top_right[1] >= half_height:
+            resp[name] = return_string.format(name, 'bottom left')
+        # now to check for intersecting quadrants
 
-    # rip these if statements. very rudimentary way to find out in which quadrant the object is in
-    if bottom_right[0] < half_width and bottom_right[1] < half_height:
-        # completely top left as furthest down coordinate is before 0,0 in coordinate plane
-        return jsonify({'response': return_string.format(name, 'top left')})
-    elif bottom_left[0] >= half_width and bottom_left[1] <= half_height:
-        # completely top right as furthest down coordinate is after 0,0
-        return jsonify({'response': return_string.format(name, 'top right')})
-    elif top_left[0] >= half_width and top_left[1] >= half_height:
-        # completely bottom right
-        return jsonify({'response': return_string.format(name, 'bottom right')})
-    elif top_right[0] < half_width and top_right[1] >= half_height:
-        return jsonify({'response': return_string.format(name, 'bottom left')})
-    # now to check for intersecting quadrants
+        if bottom_right[0] < half_width or top_right[0] < half_width:
+            resp[name] = return_string.format(name, 'left')
+        elif bottom_left[0] >= half_width or top_left[0] >= half_width:
+            resp[name] = return_string.format(name, 'right')
 
-    if bottom_right[0] < half_width or top_right[0] < half_width:
-        return jsonify({'response': return_string.format(name, 'left')})
-    elif bottom_left[0] >= half_width or top_left[0] >= half_width:
-        return jsonify({'response': return_string.format(name, 'right')})
+    return resp
 
 
 if __name__ == '__main__':
