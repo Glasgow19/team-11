e3811516e7ea4d26f724799db737f2f18c8415d4
Patricia milou <patriciamilou97@gmail.com>
2019-10-27 08:21:29 +0000

    frontend:new splash screen

--- frontendgsc/components/SplashScreen.js
+++ frontendgsc/components/SplashScreen.js
@@ -3,16 +3,15 @@ import { View, Image } from 'react-native';
 
 
 const SplashScreen = (props) => {
-    setTimeout( () => {
-        props.navigation.navigate('LanguagePage');
-    }, 1000);
+    // setTimeout( () => {
+    //     props.navigation.navigate('LanguagePage');
+    // }, 1000);
     return (
-        <View style = {{flex:12,
-            backgroundColor:"#ffffff",justifyContent:"center",
-            alignContent:"center", alignSelf:"center"}}>
+        <View style = {{flex:1,
+            backgroundColor:"#131313",justifyContent:"center",paddingHorizontal:100}}>
             <Image
-                style={{width: 250,resizeMode: 'contain'}}
-                source={require('../assets/logo.jpg')}
+                style={{paddingHorizontal:100,width:100,resizeMode: 'contain'}}
+                source={require('../assets/logo.png')}
             />
         </View>
     )
