269238636da24b4835d87e311e4b9eb002375a93
Hemang Kandwal <hemangkandwal@gmail.com>
2019-10-27 01:20:48 +0000

    fix directory path

--- backendgsc/machinelearningapi.py
+++ backendgsc/machinelearningapi.py
@@ -1,18 +1,5 @@
-from yolov3tf2.detect import run
 import sys
 import os
+sys.path.append(os.path.realpath('./yolov3tf2'))
 
-# Absolute Path of the module
-absFilePath = os.path.abspath(__file__)
-print(absFilePath)
-fileDir = os.path.dirname(os.path.abspath(
-    __file__))   # Directory of the Module
-print(fileDir)
-# Directory of the Module directory
-parentDir = os.path.dirname(fileDir)
-print(parentDir)
-# Get the directory for StringFunctions
-newPath = os.path.join(parentDir, 'yolov3tf2')
-print(newPath)
-# Add path into PYTHONPATH
-sys.path.append(newPath)
+import yolov3tf2
